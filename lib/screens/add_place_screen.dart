c76fc3277cc668fa85059928133a19f662f10948
FrancaTM <francatm@outlook.com>
2019-09-21 12:21:01 -0300

    add "add place" feature

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/great_places_provider.dart';
 import '../widgets/image_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
@@ -10,7 +14,21 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
-  final _textController = TextEditingController();
+  final _titleController = TextEditingController();
+  File _pickedImage;
+
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
+
+  void _savePlace() {
+    if (_titleController.text.isEmpty || _pickedImage == null) {
+      return;
+    }
+    Provider.of<GreatPlacesProvider>(context, listen: false)
+        .addPlace(_titleController.text, _pickedImage);
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,10 +47,10 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                   children: <Widget>[
                     TextField(
                       decoration: InputDecoration(labelText: 'Title'),
-                      controller: _textController,
+                      controller: _titleController,
                     ),
                     SizedBox(height: 10),
-                    ImageInput(),
+                    ImageInput(_selectImage),
                   ],
                 ),
               ),
@@ -46,7 +64,7 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
               elevation: 0,
               materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
               color: Theme.of(context).accentColor,
-              onPressed: () {},
+              onPressed: _savePlace,
             ),
           ),
         ],
