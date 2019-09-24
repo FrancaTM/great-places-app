1a789c07676b1d3c41fc8baa878bb0b313b30662
FrancaTM <francatm@outlook.com>
2019-09-24 19:14:26 -0300

    add store and fetch data features

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -19,25 +19,32 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Consumer<GreatPlacesProvider>(
-        child: Center(
-          child: const Text('Got no places yet, start adding some!'),
-        ),
-        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
-            ? ch
-            : ListView.builder(
-                itemCount: greatPlaces.items.length,
-                itemBuilder: (ctx, index) => ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: FileImage(
-                      greatPlaces.items[index].image,
-                    ),
-                  ),
-                  title: Text(greatPlaces.items[index].title),
-                  onTap: () {
-                    // go to detail page...
-                  },
+      body: FutureBuilder(
+        future: Provider.of<GreatPlacesProvider>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(child: CircularProgressIndicator())
+            : Consumer<GreatPlacesProvider>(
+                child: Center(
+                  child: const Text('Got no places yet, start adding some!'),
                 ),
+                builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+                    ? ch
+                    : ListView.builder(
+                        itemCount: greatPlaces.items.length,
+                        itemBuilder: (ctx, index) => ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: FileImage(
+                              greatPlaces.items[index].image,
+                            ),
+                          ),
+                          title: Text(greatPlaces.items[index].title),
+                          onTap: () {
+                            // go to detail page...
+                          },
+                        ),
+                      ),
               ),
       ),
     );
