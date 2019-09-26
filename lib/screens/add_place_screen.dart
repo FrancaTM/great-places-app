e57e6b771827d84fcffc0d783526237f719551f8
FrancaTM <francatm@outlook.com>
2019-09-26 01:22:45 -0300

    add location input layout

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:great_places_app/widgets/location_input.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/great_places_provider.dart';
@@ -51,6 +52,8 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                     ),
                     SizedBox(height: 10),
                     ImageInput(_selectImage),
+                    SizedBox(height: 10),
+                    LocationInput(),
                   ],
                 ),
               ),
