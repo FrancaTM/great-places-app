204769fd34d8bbe45b4a496641b595d137d49726
FrancaTM <francatm@outlook.com>
2019-09-27 13:05:00 -0300

    display static map snapshot (google maps)

---
+++ lib/helpers/location_helper.dart
@@ -0,0 +1,8 @@
+import 'package:great_places_app/helpers/api_key.dart';
+
+class LocationHelper {
+  static String generateLocationPreviewImage(
+      {double latitude, double longitude}) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_MAPS_API_KEY';
+  }
+}
